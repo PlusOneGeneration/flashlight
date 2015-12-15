77f09d1d2017fa6dd4f05180bb0853df5a337877
koralex <raver.trancer@gmail.com>
2015-12-15 15:06:16 +0200

    - testing peak detector

--- public/index.js
+++ public/index.js
@@ -32,9 +32,10 @@ var canvas = document.querySelector('.visualizer');
 var canvasCtx = canvas.getContext("2d");
 var biquadFilter = audioCtx.createBiquadFilter();
 
-biquadFilter.type = "lowshelf";
-biquadFilter.frequency = 70;
-biquadFilter.gain = 25;
+biquadFilter.type = "lowpass";
+biquadFilter.frequency = 64;
+biquadFilter.Q = 10;
+//biquadFilter.gain = 25;
 
 var convolver = audioCtx.createConvolver();
 
@@ -87,15 +88,12 @@ function visualize() {
     WIDTH = canvas.width;
     HEIGHT = canvas.height;
 
-
     var visualSetting = visualSelect.value;
 
     if (visualSetting == "frequencybars") {
-        analyser.fftSize = 2048;
+        analyser.fftSize = 4096;
         var bufferLength = analyser.frequencyBinCount;
-        console.log('bufferLength', bufferLength);
         var dataArray = new Uint8Array(bufferLength);
-        console.log('data', dataArray)
 
         canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
 
@@ -106,7 +104,12 @@ function visualize() {
 
             analyser.getByteFrequencyData(dataArray);
 
-            var c = dataArray;
+            //console.log(analyser.getByteFrequencyData(dataArray))
+            process(dataArray);
+
+
+            //var c = dataArray;
+
             //var color = 'rgb('+c[6]+','+c[6]+' ,'+ c[6]+')';
             //var diff = c[1] - prev;
             var diff = c[c.length - 1] - c[0];
@@ -170,14 +173,145 @@ function muteSound() {
     if (mute.id == "") {
         clearCanvas();
         gainNode.gain.value = 0;
-        console.log(gainNode.gain.value)
         mute.id = "activated";
         mute.innerHTML = "Unmute";
     } else {
         gainNode.gain.value = 1;
-        console.log(gainNode.gain.value)
         mute.id = "";
         mute.innerHTML = "Mute";
     }
+}
+//------------------------------------------------------------------------------
+
+
+//audio_file.onchange = function() {
+//    var file = this.files[0];
+//    var reader = new FileReader();
+//    var context = new(window.AudioContext || window.webkitAudioContext)();
+//    reader.onload = function() {
+//        context.decodeAudioData(reader.result, function(buffer) {
+//            prepare(buffer);
+//        });
+//    };
+//    reader.readAsArrayBuffer(file);
+//};
+
+//function prepare(buffer) {
+//    var offlineContext = new OfflineAudioContext(1, buffer.length, buffer.sampleRate);
+//    var source = offlineContext.createBufferSource();
+//    source.buffer = buffer;
+//    var filter = offlineContext.createBiquadFilter();
+//    filter.type = "lowpass";
+//    source.connect(filter);
+//    filter.connect(offlineContext.destination);
+//    source.start(0);
+//    offlineContext.startRendering();
+//    offlineContext.oncomplete = function(e) {
+//        process(e);
+//    };
+//}
+
+function process(e) {
+    var data = e;
+    //If you want to analyze both channels, use the other channel later
+    //var data = filteredBuffer.getChannelData(0);
+    var max = arrayMax(data);
+    var min = arrayMin(data);
+    var threshold = min + (max - min) * 0.98;
+    //console.log('threshold', threshold)
+    var peaks = getPeaksAtThreshold(data, threshold);
+    //console.log('peaks',peaks)
+    var intervalCounts = countIntervalsBetweenNearbyPeaks(peaks);
+    //console.log('intervalCounts', intervalCounts)
+    var tempoCounts = groupNeighborsByTempo(intervalCounts);
+    //console.log('tempoCounts', tempoCounts)
+    tempoCounts.sort(function (a, b) {
+        return b.count - a.count;
+    });
+    //if (tempoCounts.length) {
+    //    output.innerHTML = tempoCounts[0].tempo;
+    //}
+}
+
+function getPeaksAtThreshold(data, threshold) {
+    var peaksArray = [];
+    var length = data.length;
+    for (var i = 0; i < length;) {
+        if (data[i] > threshold) {
+            peaksArray.push(i);
+            // Skip forward ~ 1/4s to get past this peak.
+            i += 10000;
+        }
+        i++;
+    }
+    return peaksArray;
+}
+
+function countIntervalsBetweenNearbyPeaks(peaks) {
+    var intervalCounts = [];
+    peaks.forEach(function (peak, index) {
+        for (var i = 0; i < 10; i++) {
+            var interval = peaks[index + i] - peak;
+            var foundInterval = intervalCounts.some(function (intervalCount) {
+                if (intervalCount.interval === interval) return intervalCount.count++;
+            });
+            //Additional checks to avoid infinite loops in later processing
+            if (!isNaN(interval) && interval !== 0 && !foundInterval) {
+                intervalCounts.push({
+                    interval: interval,
+                    count: 1
+                });
+            }
+        }
+    });
+    return intervalCounts;
+}
+
+function groupNeighborsByTempo(intervalCounts) {
+    var tempoCounts = [];
+    intervalCounts.forEach(function (intervalCount) {
+        //Convert an interval to tempo
+        var theoreticalTempo = 60 / (intervalCount.interval / 44100);
+        theoreticalTempo = Math.round(theoreticalTempo);
+        if (theoreticalTempo === 0) {
+            return;
+        }
+        // Adjust the tempo to fit within the 90-180 BPM range
+        while (theoreticalTempo < 90) theoreticalTempo *= 2;
+        while (theoreticalTempo > 180) theoreticalTempo /= 2;
+
+        var foundTempo = tempoCounts.some(function (tempoCount) {
+            if (tempoCount.tempo === theoreticalTempo) return tempoCount.count += intervalCount.count;
+        });
+        if (!foundTempo) {
+            tempoCounts.push({
+                tempo: theoreticalTempo,
+                count: intervalCount.count
+            });
+        }
+    });
+    return tempoCounts;
+}
+
+function arrayMin(arr) {
+    var len = arr.length,
+        min = Infinity;
+    while (len--) {
+        if (arr[len] < min) {
+            min = arr[len];
+        }
+    }
+    return min;
+}
+
+function arrayMax(arr) {
+    var len = arr.length,
+        max = -Infinity;
+    while (len--) {
+        if (arr[len] > max) {
+            max = arr[len];
+        }
+    }
+    return max;
 }
 
